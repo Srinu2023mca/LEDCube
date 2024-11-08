f01d23739f1df5e43648456862628c4491b4c0e6
Srinu2023mca <132866061+Srinu2023mca@users.noreply.github.com>
2024-11-08 17:50:37 +0530

    changes done

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const Axios = axios.create({
-    baseURL: 'http://localhost:5002/', // Replace with your API's base URL
+    // baseURL: 'http://localhost:5002/', // Replace with your API's base URL
+    baseURL: 'https://ledcube-server.onrender.com/',
     headers: {
       'Content-Type': 'application/json',
     },
