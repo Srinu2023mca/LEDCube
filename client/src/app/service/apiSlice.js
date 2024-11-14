1702c45293c6efbe9328525d7d52d03c6e316e16
Srinu2023mca <132866061+Srinu2023mca@users.noreply.github.com>
2024-11-14 18:10:10 +0530

    update url

--- client/src/app/service/apiSlice.js
+++ client/src/app/service/apiSlice.js
@@ -3,8 +3,8 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
 // console.log(import.meta.env.VITE_BASE_URL);
-const url='http://localhost:5002' ;
-// const url='https://ledcube-server.onrender.com' ;
+// const url='http://localhost:5002' ;
+const url='https://ledcube-server.onrender.com' ;
 
 const baseQuery = fetchBaseQuery({
   baseUrl: url,
