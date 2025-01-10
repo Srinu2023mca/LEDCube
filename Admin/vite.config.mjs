f8597e29ee6b015088a05e4ee96abadfd3cb291c
aishastri <132866061+Srinu2023mca@users.noreply.github.com>
2025-01-10 20:05:06 +0530

    to fix an error

--- Admin/vite.config.mjs
+++ Admin/vite.config.mjs
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig(({ mode }) => {
   // const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: './',
+    base: '/',
     build: {
       outDir: 'dist',
     },
